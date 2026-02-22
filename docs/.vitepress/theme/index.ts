68351d89d944e85f783dc2ee13d26b846f4c131f
xianluo117 <807583778@qq.com>
2026-02-22 21:56:19 +0800

    docs: 更新故障排查并新增全局顶部提示横幅

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,29 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
 import "./custom.css";
 
 export default {
   extends: DefaultTheme,
+  Layout: () => {
+    const banner = () =>
+      h("div", { class: "global-top-banner-row" }, [
+        h("div", { class: "global-top-banner" }, [
+          "若未查找到对应内容请前往",
+          h(
+            "a",
+            {
+              href: "https://discord.com/channels/1134557553011998840/1337107956499615744",
+              target: "_blank",
+              rel: "noreferrer",
+            },
+            "答疑区",
+          ),
+          "。",
+        ]),
+      ]);
+
+    return h(DefaultTheme.Layout, null, {
+      "layout-top": banner,
+    });
+  },
 };
